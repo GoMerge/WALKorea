bbaaa892456a04647df342dbb2d214b0fdf59b66
pungsuni <dlwldud000223@gmail.com>
2025-09-22 03:02:10 +0900

    main 경로 추가, gitignore 파일 내용 추가, requirements.txt 내용 추가

--- app/main.py
+++ app/main.py
@@ -1,17 +1,12 @@
-# app/main.py
 from fastapi import FastAPI
-from app.routers import hello  # 라우터 임포트
+from app.routers import auth, user
+from app.scheduler import start_scheduler
 
-app = FastAPI(
-    title="WALKorea API",
-    description="WALKorea 프로젝트용 FastAPI 서버",
-    version="0.1.0"
-)
+app = FastAPI()
 
 # 라우터 등록
-app.include_router(hello.router)
+app.include_router(auth.router, prefix="/auth")
+app.include_router(user.router, prefix="/user")
 
-# 기본 root 엔드포인트
-@app.get("/")
-def root():
-    return {"message": "Welcome to WALKorea API 🚀"}
+# 스케줄러 시작
+start_scheduler()
